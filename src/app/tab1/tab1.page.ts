6905a245f15e7f04df805b9b7268dc56f950f37b
Luis Bertocco <luis.bertocco@etec.sp.gov.br>
2021-05-26 19:48:49 -0300

    Adicionamos array de filmes e a interface de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/iFIlme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -10,7 +11,36 @@ import { ToastController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController, public toastController: ToastController) {}
+  titulo: 'Videos App1';
+
+  listaVideos: IFilme[] = [
+    {
+      nome: 'Um Sonho de Liberdade',
+      lancamento: '1994',
+      duracao: '2h 22m',
+      classificacao: 76,
+      cartaz: 'https://www.themoviedb.org/t/p/w220_and_h330_face/4BSuFpQ2gd2WtO5ZK1fMjaMPMRc.jpg',
+      generos: ['Drama', 'Crime']
+    },
+    {
+      nome: 'À Espera de Um Milagre ',
+      lancamento: '1999',
+      duracao: '3h 9m',
+      classificacao: 85,
+      cartaz: 'https://www.themoviedb.org/t/p/w220_and_h330_face/2RKjFMIlb3UmvUtxeM1uzS0WMhG.jpg',
+      generos: ['Fantasia', 'Drama', 'Crime']
+    },
+    {
+      nome: 'A Vida é Bela',
+      lancamento: '1997',
+      duracao: '1h 56m',
+      classificacao: 85,
+      cartaz: 'https://www.themoviedb.org/t/p/w220_and_h330_face/h8PEObIiYUo4wHGo2qgWge2wqzv.jpg',
+      generos: ['Comédia', 'Drama']
+    }
+  ];
+
+  constructor(public alertController: AlertController, public toastController: ToastController) { }
 
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
