5e600b222b946e0052ab97486690b746136d0f5f
Luis Bertocco <luis.bertocco@etec.sp.gov.br>
2021-06-13 10:21:26 -0300

    Tab2 (séries) completa, falta a página individual de cada série

---
+++ src/app/dados-serie/dados-serie.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dados-serie',
+  templateUrl: './dados-serie.page.html',
+  styleUrls: ['./dados-serie.page.scss'],
+})
+export class DadosSeriePage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
